5831b2627c16b722e21a7dbf570ca664207afc89
johnjoker13 <joaoavelino2008@hotmail.com>
2023-01-03 18:13:06 -0300

    build: starting spring project

---
+++ spring-class/src/main/java/com/springclass/acc/java/springclass/SpringClassApplication.java
@@ -0,0 +1,13 @@
+package com.springclass.acc.java.springclass;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringClassApplication {
+	// comment
+	public static void main(String[] args) {
+		SpringApplication.run(SpringClassApplication.class, args);
+	}
+
+}
